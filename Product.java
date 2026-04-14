21d0212a57ec74cff9f86aa64117ceba5a278d03
Saisri Gandla <gandlasaisri895@gmail.com>
2026-04-14 11:36:03 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,15 @@
+package JFS10KT4;
+public class Product {
+    private int productId;
+    private String productName;
+    private double price;
+    private int quantity;
+    public int getProductId() { return productId; }
+    public void setProductId(int productId) { this.productId = productId; }
+    public String getProductName() { return productName; }
+    public void setProductName(String productName) { this.productName = productName; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+}
